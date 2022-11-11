3512b050e1867195be028d89eaa8a6deedd99d02
madboylonely <hunglongnt93@gmail.com>
2022-11-11 17:58:16 +0700

    Change to pure-css in index-edit.html and style-edit.css

---
+++ control.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $(".comments-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 1,
+        nav: true,
+      },
+      1000: {
+        items: 1,
+        nav: true,
+        loop: true,
+      },
+    },
+  });
+});
