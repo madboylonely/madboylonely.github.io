6f12b07083daa765148930f4dd441470a4638bad
madboylonely <hunglongnt93@gmail.com>
2022-11-24 00:05:47 +0700

    add layout admin with index-admin.html and control.js

--- control.js
+++ control.js
@@ -1,22 +1,57 @@
-$(document).ready(function () {
-  $(".comments-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: true,
-      },
-      600: {
-        items: 1,
-        nav: true,
-      },
-      1000: {
-        items: 1,
-        nav: true,
-        loop: true,
-      },
-    },
-  });
-});
+import data from './data.json' assert { type: 'json' }
+
+
+
+// Querry Selector
+let headerLeftLink1 = document.querySelector("#header-left-link1");
+let headerLeftLink2 = document.querySelector("#header-left-link2");
+let headerRightLink1 = document.querySelector("#header-right-link1");
+let headerRightLink2 = document.querySelector("#header-right-link2");
+let headerRightLink3 = document.querySelector("#header-right-link3");
+let headerRightLink4 = document.querySelector("#header-right-link4");
+let mainName = document.querySelector("#main-name");
+let avatarMid1 = document.querySelector("#avatar-mid1");
+let avatarMid2 = document.querySelector("#avatar-mid2");
+let avatarLeftItem1Label = document.querySelector("#avatar-left-item1-label");
+let avatarLeftItem1Pag = document.querySelector("#avatar-left-item1-pag");
+let avatarRightItem1Label = document.querySelector("#avatar-right-item1-label");
+let avatarRightItem1Pag = document.querySelector("#avatar-right-item1-pag");
+let avatarRightItem2Label = document.querySelector("#avatar-right-item2-label");
+let avatarRightItem2Pag = document.querySelector("#avatar-right-item2-pag");
+let educationLeftLable = document.querySelector("#education-left-label");
+let educationLeftItem1Head = document.querySelector("#education-left-item1-head");
+let educationLeftItem1Pag = document.querySelector("#education-left-item1-pag");
+let educationMidLable = document.querySelector("#education-mid-label");
+let educationMidItem1Head = document.querySelector("#education-mid-item1-head");
+let educationMidItem1Pag = document.querySelector("#education-mid-item1-pag");
+let educationMidItem2Head = document.querySelector("#education-mid-item2-head");
+let educationMidItem2Pag = document.querySelector("#education-mid-item2-pag");
+let educationRightLable = document.querySelector("#education-right-label");
+let educationRightItem1Head = document.querySelector("#education-right-item1-head");
+let educationRightItem1Pag = document.querySelector("#education-right-item1-pag");
+let educationRightItem2Head = document.querySelector("#education-right-item2-head");
+let educationRightItem2Pag = document.querySelector("#education-right-item2-pag");
+let leadershipItem1Link = document.querySelector("#leadership-item1");
+let leadershipItem1Pag = document.querySelector("#leadership-item1-pag");
+let leadershipItem1Head = document.querySelector("#leadership-item1-head");
+let leadershipItem2Link = document.querySelector("#leadership-item2");
+let leadershipItem2Pag = document.querySelector("#leadership-item2-pag");
+let leadershipItem2Head = document.querySelector("#leadership-item2-head");
+let advantageItem1Icon = document.querySelector("#advantage-item1");
+let advantageItem1Score = document.querySelector("#advantage-item1-score");
+let advantageItem1Pag = document.querySelector("#advantage-item1-pag");
+let advantageItem2Icon = document.querySelector("#advantage-item2");
+let advantageItem2Score = document.querySelector("#advantage-item2-score");
+let advantageItem2Pag = document.querySelector("#advantage-item2-pag");
+let advantageItem3Icon = document.querySelector("#advantage-item3");
+let advantageItem3Score = document.querySelector("#advantage-item3-score");
+let advantageItem3Pag = document.querySelector("#advantage-item3-pag");
+let footerCopyright = document.querySelector("#footer-copyright");
+let footerEmail = document.querySelector("#footer-email");
+
+
+
+
+
+
+console.log(data);
